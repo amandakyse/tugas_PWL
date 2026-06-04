b05b15056429275b02dc36abc3cf775225b4f774
Amandakyse <amandakyse@gmail.com>
2026-06-04 13:49:28 +0800

    tugas5 WEB CRUD

---
+++ tugas5/database/migrations/2026_05_18_144242_create_detail_buku_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+    #detail buku dari buku
+
+    $buku = Buku::find(1);//select * from buku where id=1
+    
+        Schema::create('detail_buku', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('buku_id')
+                ->unique()
+                ->constrained('buku')
+                ->onDelete('cascade');
+
+            $table->string('isbn');
+            $table->integer('jumlah_halaman');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_buku');
+    }
+};
